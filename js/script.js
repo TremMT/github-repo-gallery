de8fcf0d34bc6cf94a92ecffca6f260761e7e4f1
TremMT <tremariet@gmail.com>
2023-09-08 19:12:15 -0700

    Input updated - search text changed to lowercase and completed search capabilities

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ const repoInfoSection = document.querySelector(".repos");
 // ^ Selects the section with a class of “repos” where all your repo information appears. 
 const indRepoData = document.querySelector(".repo-data");
 // ^ Selects the section with a class of “repo-data” where the individual repo data will appear.
+const backButton = document.querySelector(".view-repos")
+// select the Back to Repo Gallery button.
+const filterInput = document.querySelector(".filter-repos")
+// the input with the “Search by name” placeholder.
 
 const gHInformation = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -18,7 +22,6 @@ const gHInformation = async function () {
 };
 
 gHInformation();
-// ^ Call
 
 const displayUserInfo = function (data) {
   const div = document.createElement("div");
@@ -34,11 +37,10 @@ const displayUserInfo = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos
     }</p>
     </div> `;
-  // ^ Inside the 5 placeholders, use the JSON data to grab the relevant properties to display on the page. = add DATA
+  
   myProfile.append(div);
-  // ^ Append the div to the overview element.
+
   getRepos();
-  // ^ At the bottom of your second function which displays your GitHub user data, call the async function that fetches your repos.
 };
 
 const getRepos = async function () {
@@ -46,17 +48,16 @@ const getRepos = async function () {
   const getRepoData = await repos.json();
   //console.log(getRepoData);
   displayRepos(getRepoData);
-  // ^ At the bottom of the async function fetching your repos, call the function you just created to display info about each repository. As an argument, pass the JSON response data for the repos.
 };
 
 const displayRepos = function (repos) {
-  //  Inside the function, loop and create a list item for each repo and give each item - Since you are looping through multiple objects, use the for(...of) loop - Create a loop FOR EACH REPO OF the REPOS array
+  filterInput.classList.remove("hide");
   for (const repo of repos) {
     const eachRepo = document.createElement("li");
     eachRepo.classList.add("repo");
     eachRepo.innerHTML = `<h3>${repo.name}</h3>`;
     repoList.append(eachRepo);
-// ^ Append the list item to the global variable that selects the unordered repos list.
+
   }
 };
 
@@ -89,6 +90,7 @@ const specRepoDisplay = function (repoInfo, languages) {
   indRepoData.innerHTML = "";
   indRepoData.classList.remove("hide");
   repoInfoSection.classList.add("hide");
+  backButton.classList.remove("hide");
   const div = document.createElement("div");
   div.innerHTML = `
   <h3>Name: ${repoInfo.name}</h3>
@@ -99,4 +101,28 @@ const specRepoDisplay = function (repoInfo, languages) {
   `
   indRepoData.append(div);
 };
-// Append the new div element to the section with a class of “repo-data”. Unhide (show) the “repo-data” element. Hide the element with the class of “repos”.
+
+
+backButton.addEventListener("click", function() {
+  repoInfoSection.classList.remove("hide");
+  indRepoData.classList.add("hide");
+  backButton.classList.add("hide");
+});
+
+filterInput.addEventListener("input", function(e) {
+const search = e.target.value;
+// captures what is typed in the search box
+//console.log(search);
+const repos = document.querySelectorAll(".repo");
+const searchReposLower = search.toLowerCase();
+
+for(const repo of repos) {
+  const repoLowercase = repo.innerText.toLowerCase();
+  //console.log(repoLowercase);
+  if (repoLowercase.includes(searchReposLower)) {
+    repo.classList.remove("hide");
+  } else {
+    repo.classList.add("hide");
+  }
+}
+});
